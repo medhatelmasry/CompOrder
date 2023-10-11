d8f79bcf45568f32cb0cbba691ac94eb331aeb5b
Medhat Elmasry <medhat@elmasry.ca>
2023-10-11 10:07:06 -0700

    added contact

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,30 +23,22 @@ public class HomeController : Controller
     {
         if (ModelState.IsValid)
         {
-            // OrderEntity order = new OrderEntity();
-            // order.Email = Request.Form["Email"];
-            // order.FirstName = Request.Form["FirstName"];
-            // order.LastName = Request.Form["LastName"];
-            // order.Street = Request.Form["street"];
-            // order.City = Request.Form["city"];
-            // order.Province = Request.Form["province"];
-            // order.PostalCode = Request.Form["postalCode"];
-            // order.Phone = Request.Form["phone"];
-            // order.OrderType = Request.Form["orderType"];
-            // order.NumberOfTickets = Convert.ToInt32(Request.Form["numberOfTickets"]);
-            // order.PaymentType = Request.Form["paymentType"];
-            // order.CardType = Request.Form["cardType"];
-            // order.CardNumber = Request.Form["cardNumber"];
-            // order.ExpiryMonth = Convert.ToInt32(Request.Form["expiryMonth"]);
-            // order.ExpiryYear = Convert.ToInt32(Request.Form["expiryYear"]);
-
-            // Console.WriteLine($"++++++++++++ {order.Email}");
-            Console.WriteLine($"------------ {ordr.Email}");
-
+            //Console.WriteLine($"------------ {ordr.Email}");
             return View("OrderSummary", ordr); // pass the order object to the view
         }
         return View("Index");
     }
+
+        public IActionResult Contact(Contact contact)
+    {
+        if (ModelState.IsValid)
+        {
+            //Console.WriteLine($"------------ {ordr.Email}");
+            return View("Contact", contact); // pass the order object to the view
+        }
+        return View("Index");
+    }
+
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
---
+++ Models/Contact.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Comp1170mvc.Models
+{
+    public class Contact
+    {
+        public string? FullName { get; set; }
+        public string? Email { get; set; }
+    }
+}
